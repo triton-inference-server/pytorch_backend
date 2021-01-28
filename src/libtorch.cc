4e07c06f0bc8a6b662dd00fa1abf775e103d01b7
David Goodwin <davidg@nvidia.com>
2021-01-28 13:11:56 -0800

    Revert "Fix header includes for new repo structure (#11)"
    This reverts commit f3cc31f2cd930a3651165494cb2f72036e10f57b.

--- src/libtorch.cc
+++ src/libtorch.cc
@@ -39,7 +39,13 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsign-compare"
 #pragma warning(push, 0)
-#include <torchvision/ops/ops.h>
+#include <torchvision/DeformConv.h>
+#include <torchvision/PSROIAlign.h>
+#include <torchvision/PSROIPool.h>
+#include <torchvision/ROIAlign.h>
+#include <torchvision/ROIPool.h>
+#include <torchvision/empty_tensor_op.h>
+#include <torchvision/nms.h>
 #include <torchvision/vision.h>  // Torchvision header
 #pragma warning(pop)
 #pragma GCC diagnostic pop
