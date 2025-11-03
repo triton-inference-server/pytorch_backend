1d5092ea52a1164f6901cdbcb1056a8f47e70453
J Wyman <jwyman@nvidia.com>
2025-11-03 15:04:07 -0500

    Accept Rename
    Renamed 'string_utilities.*' to 'string_utils.*' as requested.

--- src/string_utils.cc
+++ src/string_utils.cc
@@ -24,7 +24,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "string_utilities.hh"
+#include "string_utils.hh"
 
 
 namespace triton::backend::pytorch {
